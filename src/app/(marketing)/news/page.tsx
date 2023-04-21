8ff11de4acaaecc26926eed125e84fc0d996ff1c
Toma Rosa <toma.rosa@protonmail.com>
2023-04-21 17:40:39 +0200

    chore: remove unused code

--- src/app/(marketing)/news/page.tsx
+++ src/app/(marketing)/news/page.tsx
@@ -1,27 +1,8 @@
-// import client from '@/lib/sanity.client'
-// import { urlForImage } from '@/lib/sanity.image'
-// import { groq } from 'next-sanity'
-import Image from "next/image";
-import Link from "next/link";
-// import Date from '@/components/ui/date'
-
 export const metadata = {
   title: "Jane Do - News",
 };
 
 export default async function NewsPage() {
-  // const posts = await client!.fetch(
-  //   groq`*[_type=="article" && defined(slug.current)] | order(date desc) {
-  //     _id,
-  //     name,
-  //     title,
-  //     date,
-  //     coverImage,
-  //     "slug": slug.current,
-  //     "author": author->{name, image},
-  //   }`
-  // )
-
   return (
     <section>
       <h1 className="scroll-m-20 py-24 text-center text-4xl font-extrabold tracking-tight lg:text-6xl">
